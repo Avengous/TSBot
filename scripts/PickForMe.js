81898a73bf29ca2f7f655e2a9d3545902990ba3c
Avengous <xxstryderxx@hotmail.com>
2016-11-22 19:19:55 +0000

    PickForMe 1.0

--- scripts/PickForMe.js
+++ scripts/PickForMe.js
@@ -17,60 +17,55 @@
  * @author Luigi Giovanni Schuoler <xxstryderxx@hotmail.com>
  *
  */
- 
+
 registerPlugin({
   name: 'Pick For Me LOL',
   version: '1.0',
   description: 'Picks a random League of Legends champion for you.',
   author: 'Avengous',
   vars: {
- 		command: {
-			title: 'Command String',
-			type: 'string',
- 		},
-		apiKey: {
-			title: 'Riot API Key',
-			type: 'string',
-		},
- 		message: {
-			title: 'The message that should be displayed. (%n = nick, %r = result)',
-			type: 'string'
- 		}
+                command: {
+                        title: 'Command String',
+                        type: 'string',
+                },
+                apiKey: {
+                        title: 'Riot API Key',
+                        type: 'string',
+                },
+                message: {
+                        title: 'The message that should be displayed. (%n = nick, %r = result)',
+                        type: 'string'
+                }
   }
- 
- },	function (sinusbot, config)	{
-		var get_champions_url = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key={api_key}'
 
-		function getChampionList() {
-			sinusbot.http({
-				method: 'GET',
-				url: get_champions_url.replace('{api_key}', config.apiKey)
-			}, function(err, res) {
-            if (err) {
-                send_msg(ev, "API Request error");
-                sinusbot.log(err);
-            }
-			else {
-                if (res.statusCode == 200) {
-                    var data = JSON.parse(res.data);
-		    return data
+ },     function (sinusbot, config){
+                var get_champions_url = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key={api_key}'
+				function getChampion(msg, ev) {
+					sinusbot.http({
+						"method": "GET", 
+						"url": get_champions_url.replace('{api_key}', config.apiKey)
+						"timeout": 10000, 
+						"headers":[{"Content-Type": "application/json"}]
+					}, function (error, response) {
+						var data = JSON.parse(response.data);
+						var rand_int = Math.floor(Math.random() * Object.keys(data.data).length);
+						var champions = [];
+						var i = 0;
+						for (var champion in data.data) {
+							champions[i] = champion;
+							i += 1; 
+						}
+						result = champions[rand_int];
+						msg = msg.replace('%n', ev.clientNick);
+						msg = msg.replace('%r', result);
+						sinusbot.chatChannel(msg);
+					});
 				}
-			}});
-		}
-		
- 		sinusbot.on('chat', function(ev) {
-			if(ev.msg == '-pickforme'){				
-				var msg = config.message;
-				var data = getChampionList()	
-				for (champion in data['data']) {
-					chatChannel(champion);
-				}
-				
-				result = 'TestString';
-				msg = msg.replace('%n', ev.clientNick);
-				msg = msg.replace('%r', result);					
-				chatChannel(msg);
-			}
- 		 });
 
- 	 });
+                sinusbot.on('chat', function(ev) {
+                        if(ev.msg == '!pickforme'){
+                                getChampion(config.message, ev);
+                        }
+                 });
+
+         });
